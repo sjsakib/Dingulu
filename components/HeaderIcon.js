6c41e9b71236934f2b56832fde514e65f171cdfe
sjsakib <sjsakib.bd@gmail.com>
2018-07-21 14:20:51 +0600

    minor style fix

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -4,7 +4,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const HeaderIcon = ({ navigation }) => (
     <TouchableOpacity onPress={() => navigation.openDrawer()}>
-        <Icon style={{marginLeft: 10}} name="menu" size={20}/>
+        <Icon name="menu" size={20}/>
     </TouchableOpacity>
 )
 
